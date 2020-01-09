5b2758f3563a7caa4ac146fa9d92a8550025fe3c
DangThanhTrung1999 <pumaover@gmail.com>
2020-01-09 18:06:55 +0700

    Updates

---
+++ precache-manifest.a0164a41a2b6e66bc7669335bb697d74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac4deef495b45c2d7f8eee2e59a76ae",
+    "url": "/learn_router/index.html"
+  },
+  {
+    "revision": "847d3300fbb177c94c09",
+    "url": "/learn_router/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4c5a5e7ab798ed0dd59e",
+    "url": "/learn_router/static/js/2.3382a31a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/learn_router/static/js/2.3382a31a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "847d3300fbb177c94c09",
+    "url": "/learn_router/static/js/main.c4621a7a.chunk.js"
+  },
+  {
+    "revision": "19b2e8f04ac8dabc45a0",
+    "url": "/learn_router/static/js/runtime-main.754a6d0a.js"
+  }
+]);
